e72487117abded20885034dabe0c68916a095610
rongyaoshende <rongyaoshende@outlook.com>
2025-03-15 15:32:15 +0800

    Create 0.00.3alpha.py

---
+++ 0.00.3alpha.py
@@ -0,0 +1,7 @@
+print('RojangyPythonStart Ver0.00.3')
+print('（c）Rojangy 2025。保留所有权利。')
+while True:
+    a=input('>>')
+    if a=='ver':
+        print('RojangyPythonStart Ver0.00.3')
+        print('（c）Rojangy 2025。保留所有权利。')
